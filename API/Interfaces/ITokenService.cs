f28fa11de5d1657480dc29b9d609095bee566a5e
Homer-Mctavish <49701255+Homer-Mctavish@users.noreply.github.com>
2021-10-11 10:51:57 -0400

    Add files via upload

--- API/Interfaces/ITokenService.cs
+++ API/Interfaces/ITokenService.cs
@@ -1,9 +1,10 @@
-using API.Controllers.Entities;
+using System.Threading.Tasks;
+using API.Entities;
 
 namespace API.Interfaces
 {
     public interface ITokenService
     {
-        string CreateToken(AppUser user);
+        Task<string> CreateToken(AppUser user);
     }
 }
